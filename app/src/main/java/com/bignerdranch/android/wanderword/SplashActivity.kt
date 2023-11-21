454df854a035b9c168cae53f4974602788523c94
JaStaGa <jaylinstarlinganaway@gmail.com>
2023-11-21 02:12:52 -0500

    Splash leads to a Main/Login activity

--- app/src/main/java/com/bignerdranch/android/wanderword/SplashActivity.kt
+++ app/src/main/java/com/bignerdranch/android/wanderword/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.bignerdranch.android.wanderword
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
@@ -13,8 +14,8 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        // Use a Handler to delay the transition to the main activity
-        Handler().postDelayed({
+        // Use a Handler with the main looper to delay the transition to the main activity
+        Handler(Looper.getMainLooper()).postDelayed({
             // Start the main activity after the splash delay
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
             finish() // Close the splash activity so it's not in the back stack
