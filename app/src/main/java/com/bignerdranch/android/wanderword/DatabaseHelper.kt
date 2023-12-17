3c4ad1e51bd253c8312e61127de6bd24d08e698d
JaStaGa <jaylinstarlinganaway@gmail.com>
2023-12-16 23:35:12 -0500

    added 2 files for database

---
+++ app/src/main/java/com/bignerdranch/android/wanderword/DatabaseHelper.kt
@@ -0,0 +1,72 @@
+package com.bignerdranch.android.wanderword
+
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    // Database schema constants
+    companion object {
+        const val DATABASE_NAME = "wanderword_database.db"
+        const val DATABASE_VERSION = 1
+
+        const val TABLE_USERS = "users"
+        const val COLUMN_ID = "id"
+        const val COLUMN_EMAIL = "email"
+        const val COLUMN_PASSWORD = "password"
+
+        // SQL query to create the Users table
+        const val CREATE_USERS_TABLE = "CREATE TABLE $TABLE_USERS (" +
+                "$COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "$COLUMN_EMAIL TEXT," +
+                "$COLUMN_PASSWORD TEXT)"
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        // Create tables and initial data
+        db.execSQL(CREATE_USERS_TABLE)
+        // Add more tables as needed
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        // Handle upgrades, e.g., alter table structure or migrate data
+    }
+
+    fun insertUser(email: String, password: String) {
+        val db = writableDatabase
+        val values = ContentValues().apply {
+            put(COLUMN_EMAIL, email)
+            put(COLUMN_PASSWORD, password)
+        }
+        db.insert(TABLE_USERS, null, values)
+        db.close()
+    }
+
+    @SuppressLint("Range")
+    fun getUserByEmail(email: String): User? {
+        val db = readableDatabase
+        val cursor = db.query(
+            TABLE_USERS,
+            null,
+            "$COLUMN_EMAIL = ?",
+            arrayOf(email),
+            null,
+            null,
+            null
+        )
+        val user = if (cursor != null && cursor.moveToFirst()) {
+            User(
+                cursor.getLong(cursor.getColumnIndex(COLUMN_ID)),
+                cursor.getString(cursor.getColumnIndex(COLUMN_EMAIL)),
+                cursor.getString(cursor.getColumnIndex(COLUMN_PASSWORD))
+            )
+        } else {
+            null
+        }
+        cursor?.close()
+        db.close()
+        return user
+    }
+}
---
+++ app/src/main/java/com/bignerdranch/android/wanderword/User.kt
@@ -0,0 +1,7 @@
+package com.bignerdranch.android.wanderword
+
+data class User (
+    val id: Long,
+    val email: String,
+    val password: String
+)
